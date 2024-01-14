3065db72be35e4a806ba6e06b76912d17d3a2ea3
stefan022 <stefan022github@gmail.com>
2024-01-14 15:33:54 +0100

    useResource

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useStore } from "@builder.io/qwik";
+import { component$, useResource$, useStore } from "@builder.io/qwik";
 import { Button, Counter, Title, User } from "~/components";
 import type { IUser } from "~/typescript/interfaces/IUser";
 
@@ -8,6 +8,18 @@ export default component$(() => {
 		username: "stefan022",
 		profession: "FrontEnd developer",
 		framework: "Qwik",
+	});
+
+	useResource$(() => {
+		/*
+			Use useResource$() to create a computed value that is derived asynchronously. It's 
+			the asynchronous version of useComputed$(), which includes the state of the 
+			resource (loading, resolved, rejected) on top of the value.
+
+			A common use case for useResource$() is to fetch data from an external API within 
+			the component itself, this means that the execution might happen in the server or 
+			the client.
+		*/
 	});
 
 	return (
