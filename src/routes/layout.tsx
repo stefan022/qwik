d02b02f969410c3ded123ac177dfe8525aedef69
stefan022 <stefan022github@gmail.com>
2024-01-14 17:01:35 +0100

    slot

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,18 +1,20 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 
 import { Header, Footer } from "~/components";
+import Container from "~/container/container";
+
 import styles from "./styles.css?inline";
 
 export default component$(() => {
 	useStyles$(styles);
 
 	return (
-		<>
+		<Container>
 			<Header />
 			<main>
 				<Slot />
 			</main>
 			<Footer />
-		</>
+		</Container>
 	);
 });
