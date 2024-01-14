eacf688dfd9ebc915423bd9ce13a0e348cae8c81
stefan022 <stefan022github@gmail.com>
2024-01-14 16:03:25 +0100

    event target

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,7 +3,12 @@ import { $, component$, useSignal } from "@builder.io/qwik";
 export default component$(() => {
 	const text = useSignal<string>("Click me");
 
-	const printMessage = $(() => console.log("CLICKED!"));
+	// Because event handling is asynchronous, you can't use event.currentTarget.
+	// To solve this Qwik handlers provide a currentTarget as a second argument.
+	const printMessage = $((event: MouseEvent, currentTarget: Element) =>
+		console.log("CLICKED!", event, currentTarget)
+	);
+
 	const printSmile = $(() => console.log("xd"));
 
 	return (
