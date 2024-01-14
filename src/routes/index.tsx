14aaedb60501eac0d8553ac6c9d83b86355238d5
stefan022 <stefan022github@gmail.com>
2024-01-14 16:37:21 +0100

    useOn()

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { component$, useContextProvider, useStore } from "@builder.io/qwik";
 import { Button, Counter, Title, User } from "~/components";
 import { ctx } from "~/context/ctx";
+import { useMousePosition } from "~/hooks/useMousePosotion";
 import type { IUser } from "~/typescript/interfaces/IUser";
 import type { Theme } from "~/typescript/types/Theme";
 
 export default component$(() => {
+	const { x, y } = useMousePosition();
 	const theme = useStore<{ theme: Theme }>({ theme: "dark" });
 
 	const user = useStore<IUser>({
@@ -22,6 +24,15 @@ export default component$(() => {
 			<Button />
 			<User user={user} />
 			<Counter />
+			<div class="mouse">
+				<p>Hover me</p>
+				<div>
+					<p>Mouse position:</p>
+					<p>
+						{x},{y}
+					</p>
+				</div>
+			</div>
 		</>
 	);
 });
