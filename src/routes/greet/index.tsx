6fb201e54a116867df1773da134ca4f641a617e2
stefan022 <stefan022github@gmail.com>
2024-01-17 20:14:01 +0100

    method

--- src/routes/greet/index.tsx
+++ src/routes/greet/index.tsx
@@ -1,9 +1,9 @@
 import { type RequestHandler } from "@builder.io/qwik-city";
 
-export const onGet: RequestHandler = async ({ cookie, json }) => {
+export const onGet: RequestHandler = async ({ method, cookie, json }) => {
 	cookie.set("Qwik.cookie", "XyZ9qW1kOa");
 
 	const qwik_cookie = cookie.get("Qwik.cookie");
 
-	json(200, { greet: "hello qwik!1 xd", qwik_cookie });
+	json(200, { method, greet: "hello qwik!1 xd", qwik_cookie });
 };
