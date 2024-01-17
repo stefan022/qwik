061cbffda27ba83cd7aca627fee748ba17f37ee2
stefan022 <stefan022github@gmail.com>
2024-01-17 19:48:08 +0100

    middleware

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,9 +1,16 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
+import type { RequestHandler } from "@builder.io/qwik-city";
 
 import { Header, Footer } from "~/components";
 import Container from "~/container/container";
 
 import styles from "./styles.css?inline";
+
+export const onRequest: RequestHandler = async ({ next, url }) => {
+	console.log("Before request", url);
+	await next();
+	console.log("After request", url);
+};
 
 export default component$(() => {
 	useStyles$(styles);
