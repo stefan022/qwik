f50434128d6b1c018736c6cc8e62d33ed08d2125
stefan022 <stefan022github@gmail.com>
2024-01-17 21:51:29 +0100

    request

--- src/routes/greet/[value]/index.tsx
+++ src/routes/greet/[value]/index.tsx
@@ -8,6 +8,7 @@ export const onGet: RequestHandler = async ({
 	query,
 	params,
 	json,
+	request,
 }) => {
 	cacheControl({
 		immutable: true,
@@ -21,6 +22,7 @@ export const onGet: RequestHandler = async ({
 	// e.g.-> ?country=denmark&lang=en&weather=cloudy
 	query.forEach((v, k) => (obj[k] = v));
 	headers.forEach((v, k) => (obj[k] = v));
+	request.headers.forEach((v, k) => (obj[k] = v));
 
 	const plt = Object.keys(platform);
 
