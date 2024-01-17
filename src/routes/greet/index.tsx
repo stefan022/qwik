8a922ce5ada03e7ae0ff9cea943658230d4ba82e
stefan022 <stefan022github@gmail.com>
2024-01-17 20:17:50 +0100

    basePathname

--- src/routes/greet/index.tsx
+++ src/routes/greet/index.tsx
@@ -1,12 +1,19 @@
 import { type RequestHandler } from "@builder.io/qwik-city";
 
-export const onGet: RequestHandler = async ({ url, method, cookie, json }) => {
+export const onGet: RequestHandler = async ({
+	url,
+	basePathname: base_pathname,
+	method,
+	cookie,
+	json,
+}) => {
 	cookie.set("Qwik.cookie", "XyZ9qW1kOa");
 
 	const qwik_cookie = cookie.get("Qwik.cookie");
 
 	json(200, {
 		url: url.toString(),
+		base_pathname,
 		method,
 		greet: "hello qwik!1 xd",
 		qwik_cookie,
