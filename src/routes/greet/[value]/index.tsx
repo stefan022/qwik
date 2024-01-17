506a409ffb1f9fee2f8990e4b1f0da93ed9d26bd
stefan022 <stefan022github@gmail.com>
2024-01-17 20:44:59 +0100

    query

--- src/routes/greet/[value]/index.tsx
+++ src/routes/greet/[value]/index.tsx
@@ -1,5 +1,10 @@
 import { type RequestHandler } from "@builder.io/qwik-city";
 
-export const onGet: RequestHandler = async ({ params, json }) => {
-	json(200, { params });
+export const onGet: RequestHandler = async ({ query, params, json }) => {
+	const obj: Record<string, string> = {};
+
+	// e.g.-> ?country=denmark&lang=en&weather=cloudy
+	query.forEach((v, k) => (obj[k] = v));
+
+	json(200, { ...params, ...obj });
 };
